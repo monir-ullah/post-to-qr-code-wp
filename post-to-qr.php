e0ff81c00b96c8c458421d024de46748ff8c4428
Monir Ullah <mullah725@gmail.com>
2023-07-14 20:12:13 +0600

    plugins settings option Added

--- post-to-qr.php
+++ post-to-qr.php
@@ -24,17 +24,77 @@ add_action('plugins_loaded', 'ptoqr_load_plugin_textdomain');
 function ptoqr_initial_func($content){
     $ptoqr_post_id = get_post();
     $ptoqr_post_url = get_permalink($ptoqr_post_id->ID);
-    $image_size = apply_filters( 'ptoqr_change_image_size', '250x250' );
-    $img = '<img src = "https://api.qrserver.com/v1/create-qr-code/?size=' . $image_size .'&data=' . $ptoqr_post_url . '">';
-    
+    $height = get_option( 'ptoqr_image_heigth' );
+    $width = get_option( 'ptoqr_image_width' );
+    $height = $height ? $height : 100;
+    $width = $width ? $width : 100;
+    $image_size = apply_filters( 'ptoqr_change_image_size', "{$width}x{$height}" );
+    $img = '<img src = "https://api.qrserver.com/v1/create-qr-code/?size=' . $image_size .'&data=' . $ptoqr_post_url . '">';  
     return $content .= $img;
 
 }
 add_filter( 'the_content', 'ptoqr_initial_func' );
 
 function ptoqr_change_image_size_func($size){
-    echo "Hello World";
-    return '150x150';
+    $height = get_option( 'ptoqr_image_heigth' );
+    $width = get_option( 'ptoqr_image_width' );
+    $full_value = $width . 'x' . $height;
+    echo $full_value;
+    return $full_value;
 }
 
 add_filter( 'ptoqr_change_image_size', 'ptoqr_change_image_size_func' );
+
+
+
+
+// Post to QR COde Plugin Settigs Section
+
+add_action( 'admin_init', 'ptoqr_setting_option' );
+
+function ptoqr_setting_option(){
+    // Plugins Setting Sectiton Title
+    add_settings_section( 
+        'ptoqr_seting_option_page',
+        __('Post to QR Code Setting Option'),
+        'atoqr_add_settings_section_title',
+        'general'
+    );
+
+    // Setting Field Width
+    add_settings_field( 
+        'ptoqr_image_width',
+        __('QR Code Width ','ptoqr'),
+        'ptoqr_setting_input_filed_function',
+        'general',
+        'ptoqr_seting_option_page',
+        array('ptoqr_image_width')
+    );
+
+    register_setting( 'general', 'ptoqr_image_width');
+
+
+    // Setting Filed Height
+    add_settings_field( 
+        'ptoqr_image_heigth',
+        __('QR Code Hight','ptoqr'),
+        'ptoqr_setting_input_filed_function',
+        'general',
+        'ptoqr_seting_option_page',
+        array('ptoqr_image_heigth')
+    );
+    register_setting( 'general', 'ptoqr_image_heigth');
+}
+
+function atoqr_add_settings_section_title(){
+    echo "Post to QR Code All Settings Here";
+}
+
+
+// Settings Input Field Funcwtion
+
+function ptoqr_setting_input_filed_function($args){
+    $filed_option = get_option( $args[0] );
+    printf('<input type="text" name="%s" id="%s" value="%s">',$args[0],$args[0], $filed_option );  
+}
+
