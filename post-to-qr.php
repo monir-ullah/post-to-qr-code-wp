ffe8b7987e1a8afb5827832e911c4797b40ec82f
Monir Ullah <mullah725@gmail.com>
2023-07-15 00:42:46 +0600

    15/7/2023

--- post-to-qr.php
+++ post-to-qr.php
@@ -19,7 +19,26 @@ function ptoqr_load_plugin_textdomain(){
 }
 add_action('plugins_loaded', 'ptoqr_load_plugin_textdomain');
 
+// Country List
+$ptoqr_country_list = array(
 
+    __( 'Afganistan', 'posts-to-qrcode' ),
+    __( 'Bangladesh', 'posts-to-qrcode' ),
+    __( 'Bhutan', 'posts-to-qrcode' ),
+    __( 'India', 'posts-to-qrcode' ),
+    __( 'Maldives', 'posts-to-qrcode' ),
+    __( 'Nepal', 'posts-to-qrcode' ),
+    __( 'Pakistan', 'posts-to-qrcode' ),
+    __( 'Sri Lanka', 'posts-to-qrcode' ),
+
+);
+
+function ptoqr_country_hook_enable(){
+
+    global $ptoqr_country_list;
+    $ptoqr_country_list = apply_filters( 'ptoqr_country_list_hook', $ptoqr_country_list );
+}
+add_action( 'init', 'ptoqr_country_hook_enable' );
 // initial function 
 function ptoqr_initial_func($content){
     $ptoqr_post_id = get_post();
@@ -57,7 +76,7 @@ function ptoqr_setting_option(){
     add_settings_section( 
         'ptoqr_seting_option_page',
         __('Post to QR Code Setting Option'),
-        'atoqr_add_settings_section_title',
+        'atoqr_add_settings_section_title_func',
         'general'
     );
 
@@ -74,6 +93,30 @@ function ptoqr_setting_option(){
     register_setting( 'general', 'ptoqr_image_heigth_and_width');
 
 
+    // Setting Select Country  Option
+
+    add_settings_field( 
+        'ptoqr_country_select',
+        __('Select Country'),
+        'ptoqr_country_select_func',
+        'general',
+        'ptoqr_seting_option_page', 
+    );
+
+    register_setting( 'general', 'ptoqr_country_select' );
+
+    // Setting Checkbox Country  Option
+
+    add_settings_field( 
+        'ptoqr_country_checkbox',
+        __('Select Country from List'),
+        'ptoqr_country_checkbox_func',
+        'general',
+        'ptoqr_seting_option_page', 
+    );
+
+    register_setting( 'general', 'ptoqr_country_checkbox' );
+
     // Setting Filed Height
     /*
     add_settings_field( 
@@ -88,7 +131,7 @@ function ptoqr_setting_option(){
     */
 }
 
-function atoqr_add_settings_section_title(){
+function atoqr_add_settings_section_title_func(){
     echo "Post to QR Code All Settings Here";
 }
 
@@ -100,3 +143,44 @@ function ptoqr_setting_input_filed_function($args){
     printf('<input type="number" name="%s" id="%s" value="%s">',$args[0],$args[0], $filed_option );  
 }
 
+// Select Filed Function
+
+function ptoqr_country_select_func(){
+    global $ptoqr_country_list;
+    $select_option = get_option( 'ptoqr_country_select');
+    printf('<select name="ptoqr_country_select" id="ptoqr_country_select">');
+    foreach($ptoqr_country_list as $country){
+        $selected = '';
+        if($select_option == $country  ){
+            $selected = 'selected';
+        }
+        printf('<option value="%s" %s >%s</option>',$country,$selected,$country);
+    }
+   echo '</select>';
+
+}
+
+// Checkbox Field
+
+function ptoqr_country_checkbox_func(){
+    global $ptoqr_country_list;
+   printf('<p>Choose Countries from belloow</p></br>');
+    
+    $seleced_checkbox = get_option( 'ptoqr_country_checkbox');
+    foreach($ptoqr_country_list as $country){
+        $checked = '';
+        if( is_array($seleced_checkbox) && in_array($country, $seleced_checkbox)){
+            $checked = 'checked';
+        }
+        // here name="ptoqr_country_checkbox[]" must need . because of it's a array
+        printf(' <input type="checkbox" name="ptoqr_country_checkbox[]" value="%s" %s> %s </br>', $country, $checked, $country);
+    }
+    
+}
+
+// Contry hook testing
+function qtoqr_country_llist_testing($countries){
+    array_push($countries, __('Oman','ptoqr') );
+    return $countries;
+}
+add_action('ptoqr_country_list_hook', 'qtoqr_country_llist_testing');
